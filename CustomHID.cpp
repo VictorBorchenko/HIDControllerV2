6239db4598399a82f10a50bf176cc4c7b9a362f7
VictorBorchenko <borchenko@gmail.com>
2025-07-16 12:58:05 +0300

    feature report "0"+16bytes

--- CustomHID.cpp
+++ CustomHID.cpp
@@ -1,27 +1,29 @@
 #include "CustomHID.h"
 
 CustomHID_::CustomHID_()
-  : PluggableUSBModule(2, 1, epType) {
+    : PluggableUSBModule(2, 1, epType)
+{
   epType[0] = EP_TYPE_INTERRUPT_IN;
   epType[1] = EP_TYPE_INTERRUPT_OUT;
   PluggableUSB().plug(this);
   outCallback = nullptr;
+  memset(featureReportBuffer, 0, sizeof(featureReportBuffer));
 }
 
-int CustomHID_::getInterface(uint8_t* interfaceCount) {
+int CustomHID_::getInterface(uint8_t *interfaceCount)
+{
   *interfaceCount += 1;
   // Interface descriptor (9 bytes)
   uint8_t interfaceDesc[] = {
-    9, 4, pluggedInterface, 0x00, 1, 0x03, 0x00, 0x00, 0
-  };
+      9, 4, pluggedInterface, 0x00, 1, 0x03, 0x00, 0x00, 0};
   // HID descriptor (9 bytes)
   uint8_t hidDesc[] = {
-    9, 0x21, 0x11, 0x01, 0x00, 0x01, 0x22, (uint8_t)(sizeof(customHIDReportDescriptor)), (uint8_t)(sizeof(customHIDReportDescriptor) >> 8)
-  };
+      9, 0x21, 0x11, 0x01, 0x00, 0x01, 0x22,
+      (uint8_t)(sizeof(customHIDReportDescriptor)),
+      (uint8_t)(sizeof(customHIDReportDescriptor) >> 8)};
   // Endpoint IN (7 bytes)
   uint8_t epInDesc[] = {
-    7, 5, (uint8_t)(0x80 | pluggedEndpoint), 0x03, USB_EP_SIZE, 0, 0x01
-  };
+      7, 5, (uint8_t)(0x80 | pluggedEndpoint), 0x03, USB_EP_SIZE, 0, 0x01};
 
   USB_SendControl(0, interfaceDesc, sizeof(interfaceDesc));
   USB_SendControl(0, hidDesc, sizeof(hidDesc));
@@ -29,32 +31,60 @@ int CustomHID_::getInterface(uint8_t* interfaceCount) {
   return sizeof(interfaceDesc) + sizeof(hidDesc) + sizeof(epInDesc);
 }
 
-int CustomHID_::getDescriptor(USBSetup& setup) {
-  if (setup.wValueH == HID_REPORT_DESCRIPTOR_TYPE) {
+int CustomHID_::getDescriptor(USBSetup &setup)
+{
+  if (setup.wValueH == HID_REPORT_DESCRIPTOR_TYPE)
+  {
     return USB_SendControl(TRANSFER_PGM, customHIDReportDescriptor, sizeof(customHIDReportDescriptor));
   }
   return 0;
 }
 
-bool CustomHID_::setup(USBSetup& setup) {
-  if (pluggedInterface != setup.wIndex) return false;
-  if (setup.bmRequestType == REQUEST_DEVICETOHOST_STANDARD_INTERFACE && setup.bRequest == HID_GET_REPORT) {
-    // Host requests current report
+bool CustomHID_::setup(USBSetup &setup)
+{
+  if (pluggedInterface != setup.wIndex)
+    return false;
+
+  if (setup.bmRequestType == REQUEST_DEVICETOHOST_STANDARD_INTERFACE && setup.bRequest == HID_GET_REPORT)
+  {
     return USB_SendControl(0, reportBuffer, sizeof(reportBuffer)) >= 0;
   }
-  if (setup.bmRequestType == REQUEST_HOSTTODEVICE_CLASS_INTERFACE && setup.bRequest == HID_SET_REPORT) {
-    uint8_t outBuf[4];
-    if (USB_RecvControl(outBuf, 4) == 4 && outCallback) {
-      uint32_t value =
-        (uint32_t)outBuf[0] | ((uint32_t)outBuf[1] << 8) | ((uint32_t)outBuf[2] << 16) | ((uint32_t)outBuf[3] << 24);
-      outCallback(value);
+
+  if (setup.bmRequestType == REQUEST_HOSTTODEVICE_CLASS_INTERFACE && setup.bRequest == HID_SET_REPORT)
+  {
+    if (setup.wValueH == 3)
+    { // Feature report
+      uint8_t buf[16];
+      if (USB_RecvControl(buf, sizeof(buf)) == sizeof(buf))
+      {
+        memcpy(featureReportBuffer, buf, sizeof(buf));
+      }
+      return true;
     }
-    return true;
+    else
+    {
+      uint8_t outBuf[4];
+      if (USB_RecvControl(outBuf, 4) == 4 && outCallback)
+      {
+        uint32_t value =
+            (uint32_t)outBuf[0] | ((uint32_t)outBuf[1] << 8) |
+            ((uint32_t)outBuf[2] << 16) | ((uint32_t)outBuf[3] << 24);
+        outCallback(value);
+      }
+      return true;
+    }
   }
+
+  if (setup.bmRequestType == REQUEST_DEVICETOHOST_CLASS_INTERFACE && setup.bRequest == HID_GET_REPORT && setup.wValueH == 3)
+  {
+    return USB_SendControl(0, featureReportBuffer, sizeof(featureReportBuffer)) >= 0;
+  }
+
   return false;
 }
 
-void CustomHID_::sendReport(uint8_t b1, uint8_t b2, uint16_t bools) {
+void CustomHID_::sendReport(uint8_t b1, uint8_t b2, uint16_t bools)
+{
   reportBuffer[0] = b1;
   reportBuffer[1] = b2;
   reportBuffer[2] = bools & 0xFF;
@@ -62,6 +92,17 @@ void CustomHID_::sendReport(uint8_t b1, uint8_t b2, uint16_t bools) {
   USB_Send(pluggedEndpoint | TRANSFER_RELEASE, reportBuffer, sizeof(reportBuffer));
 }
 
-void CustomHID_::setOutCallback(void (*cb)(uint32_t)) {
+void CustomHID_::setOutCallback(void (*cb)(uint32_t))
+{
   outCallback = cb;
+}
+
+void CustomHID_::setFeatureReport(const uint8_t *data, size_t len)
+{
+  memcpy(featureReportBuffer, data, min(len, sizeof(featureReportBuffer)));
+}
+
+void CustomHID_::getFeatureReport(uint8_t *data, size_t len)
+{
+  memcpy(data, featureReportBuffer, min(len, sizeof(featureReportBuffer)));
 }
--- CustomHID.h
+++ CustomHID.h
@@ -6,30 +6,38 @@
 
 // HID Report Descriptor
 const uint8_t customHIDReportDescriptor[] PROGMEM = {
-  0x06, 0x00, 0xFF,  // Usage Page (Vendor Defined)
-  0x09, 0x01,        // Usage (Vendor Usage 1)
-  0xA1, 0x01,        // Collection (Application)
-  // OUT report (host -> device)
-  0x09, 0x02,        //   Usage (Vendor Usage 2)
-  0x15, 0x00,        //   Logical Minimum (0)
-  0x26, 0xFF, 0x00,  //   Logical Maximum (255)
-  0x75, 0x08,        //   Report Size (8)
-  0x95, 0x04,        //   Report Count (4 bytes)
-  0x91, 0x02,        //   Output (Data,Var,Abs)
-  // IN report (device -> host)
-  0x09, 0x03,  //   Usage (Vendor Usage 3)
-  0x15, 0x00,  //   Logical Minimum (0)
-  0x25, 0xFF,  //   Logical Maximum (255)
-  0x75, 0x08,  //   Report Size (8)
-  0x95, 0x02,  //   Report Count (2) - 2 bytes
-  0x81, 0x02,  //   Input (Data,Var,Abs)
-  0x09, 0x04,  //   Usage (Vendor Usage 4)
-  0x15, 0x00,  //   Logical Minimum (0)
-  0x25, 0x01,  //   Logical Maximum (1)
-  0x75, 0x01,  //   Report Size (1)
-  0x95, 0x10,  //   Report Count (16) - 16 bits
-  0x81, 0x02,  //   Input (Data,Var,Abs)
-  0xC0         // End Collection
+    0x06, 0x00, 0xFF, // Usage Page (Vendor Defined)
+    0x09, 0x01,       // Usage (Vendor Usage 1)
+    0xA1, 0x01,       // Collection (Application)
+    // OUT report (host -> device)
+    0x09, 0x02,       //   Usage (Vendor Usage 2)
+    0x15, 0x00,       //   Logical Minimum (0)
+    0x26, 0xFF, 0x00, //   Logical Maximum (255)
+    0x75, 0x08,       //   Report Size (8)
+    0x95, 0x04,       //   Report Count (4 bytes)
+    0x91, 0x02,       //   Output (Data,Var,Abs)
+    // IN report (device -> host)
+    0x09, 0x03,       //   Usage (Vendor Usage 3)
+    0x15, 0x00,       //   Logical Minimum (0)
+    0x25, 0xFF,       //   Logical Maximum (255)
+    0x75, 0x08,       //   Report Size (8)
+    0x95, 0x02,       //   Report Count (2) - 2 bytes
+    0x81, 0x02,       //   Input (Data,Var,Abs)
+    0x09, 0x04,       //   Usage (Vendor Usage 4)
+    0x15, 0x00,       //   Logical Minimum (0)
+    0x25, 0x01,       //   Logical Maximum (1)
+    0x75, 0x01,       //   Report Size (1)
+    0x95, 0x10,       //   Report Count (16) - 16 bits
+    0x81, 0x02,       //   Input (Data,Var,Abs)
+                      // Feature report (16 байт)
+    0x09, 0x05,       //   Usage (Vendor Usage 5)
+    0x15, 0x00,       //   Logical Minimum (0)
+    0x26, 0xFF, 0x00, //   Logical Maximum (255)
+    0x75, 0x08,       //   Report Size (8 бит)
+    0x95, 0x10,       //   Report Count (16 байт)
+    0xB1, 0x02,       //   Feature (Data,Var,Abs)
+    0xC0              // End Collection
+
 };
 
 #define USB_DEVICE_CLASS_HUMAN_INTERFACE 0x03
@@ -40,19 +48,21 @@ const uint8_t customHIDReportDescriptor[] PROGMEM = {
 #define HID_REPORT_DESCRIPTOR_TYPE 0x22
 
 // Custom HID class
-class CustomHID_ : public PluggableUSBModule {
+class CustomHID_ : public PluggableUSBModule
+{
 public:
   CustomHID_(void);
-  int getInterface(uint8_t* interfaceCount) override;
-  int getDescriptor(USBSetup& setup) override;
-  bool setup(USBSetup& setup) override;
+  int getInterface(uint8_t *interfaceCount) override;
+  int getDescriptor(USBSetup &setup) override;
+  bool setup(USBSetup &setup) override;
   void sendReport(uint8_t b1, uint8_t b2, uint16_t bools);
-  // void setOutCallback(void (*cb)(uint16_t));
   void setOutCallback(void (*cb)(uint32_t));
+  void setFeatureReport(const uint8_t *data, size_t len);
+  void getFeatureReport(uint8_t *data, size_t len);
+
 private:
   uint8_t epType[2];
   uint8_t reportBuffer[4];
-  //void (*outCallback)(uint16_t);
+  uint8_t featureReportBuffer[16]; // для хранения Feature Report
   void (*outCallback)(uint32_t);
 };
-
